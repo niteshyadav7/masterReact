aec3dd352fb255bbf4bf9ac888f4fffd22e32938
Nitesh Yadav <119080929+niteshyadav7@users.noreply.github.com>
2023-02-16 20:31:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import ImageList from "./component/ImageList"
-import SearchBar from "./component/SearchBar";
-import searchImages from "./api"
-import { useState } from "react";
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
-function App () {
-    const [images,setImages] =useState([]);
-    
-    const handleSubmit =async (term) => {
-        // console.log("Do a search",term);
-       const result= await searchImages(term)
-    //    console.log(results);
-    setImages(result)
-    // console.log(result);
-        
-    }
-    return (
-        <div>
-            <SearchBar onSubmit={handleSubmit} />
-            <ImageList images={images}/>
-        </div>
-    )
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ];
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
-export default App
 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
-// searchImages();
-
-
-const ele =document.getElementById('root')
-const root =ReactDOM.createRoot(ele)
-
-root.render(<App />)
-
-
-
+root.render(<App />);
